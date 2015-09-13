61d70ea85a3475e2071459b2ab5147d5f9ab5dcd
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-13 17:06:23 -0400

    Removed unused method for formatting of word quality

--- ModelingUtilities.py
+++ ModelingUtilities.py
@@ -9,34 +9,12 @@ import json
 import os
 import cPickle
 
-
 #Variable definitions relative to the path of the source files
 base_path     = os.path.dirname(__file__)
 genres        = ['children', 'crime', 'history']
 training_path = base_path + '/books/train_books/'
 test_path     = base_path + '/books/test_books/'
 special_characters = '[~!@#$%^?&*()_,.+{}":;/\']+$123456789'
-
-def formatWordForQuality(word):
-    '''
-        Replaces the special characters in the passed word with None. This is
-        done to weed out the unwanted characters
-    '''
-    
-    word = str(word)
-    
-    #Removing the special characters from the words
-    special_characters = '[~!@#$%^?&*()_,.+{}":;/\']+$123456789'
-    #word = word.translate(None, special_characters)
-    
-    #Regex method to do the same
-    word = re.sub(special_characters, '', word)
-    
-    #Converting everything to lower case
-    word = word.lower()
-    
-    return word
-
 
 def serializeUnigramModelToDisk(model, ngram):
     '''
