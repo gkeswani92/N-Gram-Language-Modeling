241daa5b3cd9ac1adfa2aa97494e326224fbfc66
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-18 19:00:16 -0400

    Classification initial commit

--- GenreClassification/KNearestNeighbourClassifier.py
+++ GenreClassification/KNearestNeighbourClassifier.py
@@ -3,3 +3,8 @@ Created on Sep 18, 2015
 
 @author: gaurav
 '''
+
+from ModelCreation_SentenceGenerator.ModelingUtilities import genres, training_path, base_path
+
+def readTextAndVectorize():
+    pass
