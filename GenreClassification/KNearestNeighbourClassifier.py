b1f6c517f8df20e136c536e15d7372f3b2d9e8e0
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-19 17:47:22 -0400

    Fixed non working version of read text and vectorize
    There seemed to be some bug with my eclipse which wasn’t working. Just
    cut paste most of your code back in.

--- GenreClassification/KNearestNeighbourClassifier.py
+++ GenreClassification/KNearestNeighbourClassifier.py
@@ -7,35 +7,51 @@ Created on Sep 18, 2015
 from ModelCreation_SentenceGenerator.ModelingUtilities 	import genres, training_path, base_path
 from collections 										import OrderedDict, Counter
 from nltk.tokenize                                      import word_tokenize
+import codecs
 
-def readTextAndVectorize(training_paths, test_paths):
-	'''
-	Reads in all text files and converts them to count vectors, represented by orderedDicts
-	'''
+def readTextAndLabels(training_paths, test_paths):
+    '''
+        Reads in all text files and converts them to count vectors, represented by orderedDicts
+    '''
+    #Variable to maintain the label for each of the training files that was read in
+    book_genre_label = {}
+    
+    #Dictionary to map the file names to the text in each file
+    tokenized_texts_dict = {}
+    for path in training_paths:
+        
+        #Storing the data against the file name
+        f = codecs.open(path,'r','utf8',errors='ignore')
+        tokenized_texts_dict[path.split('/')[-1]] = f.read()
+        print(path.split('/')[-1])
+        f.close()
+        
+        #Storing the label of the file that was read in
+        book_genre_label[path] = path.split('/')[-2]
+        
+    return tokenized_texts_dict, book_genre_label
 
-	# Dictionary mapping each training file name to the list of word tokens which occur in that file
-	tokenized_texts_dict = dict()
-	for path in training_paths:
-		f = codecs.open(file_path,'r','utf8', errors='ignore')
-    	tokenized_texts_dict.update({path.split('/').[-1]: f.read()})
-    	f.close()
+def createTrainingVectors(tokenized_texts_dict):
+    '''
+        Given the filenames and their contents, this methods creates the training 
+        vectors by creating a unique list of all words together in the training
+        set
+    '''
+    #Creating a set of all the words in the training set
+    unique_words = set([token for text in tokenized_texts_dict.values() for token in text])
+    
+    #Creating the initial vector with counts 0 for all training sets
+    zero_vector = OrderedDict(zip(unique_words,[0]*len(unique_words)))
+    
+    #For each training file, create an OrderedDict containing its word counts (together with zero counts),
+    #and store it in a dict, indexed by its corresponding filename
+    training_vectors = {}
+    for filename, token_list in tokenized_texts_dict.iteritems():
+        current_vector = zero_vector.copy()
+        current_vector.update(Counter(token_list))
+        training_vectors[filename] = current_vector
+        
+    return training_vectors
 
-    # Set of all unique words which occur in the training documents
-    unique_words = set(word for word in token_list for token_list in tokenized_texts_dict.values())
 
-    # Create an OrderedDict with the unique words as keys, and the values all being zero
-    zero_vector = OrderedDict(zip(list(unique_words),[0 for _ in range(len(unique_words))]))
-       
-    # For each training file, create an OrderedDict containing its word counts (together with zero counts),
-    # and store it in a dict, indexed by its corresponding filename
-    training_dicts = dict()
-    for filename,tokenlist in tokenized_texts_dict.iteritems():
-    	this_dict = zero_vector.copy()
-    	this_dict.update(Counter(tokenlist))
-    	training_dicts.update({filename: this_dict})
-
-    # Also create dict mapping filenames to labels
-
-    # Create a separate dict mapping the test (or validation) filenames to OrderedDicts as well,
-    # using the "zero_vector" constructed from the training files as a scaffold
-
+    
