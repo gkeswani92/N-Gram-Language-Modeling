30600c654930929580ff6d9f82a6b1a7d7fab699
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-21 20:46:56 -0400

    Smoothing Package

---
+++ Smoothing/GoodTuring.py
@@ -0,0 +1,11 @@
+'''
+Created on Sep 21, 2015
+
+@author: gaurav
+'''
+def applyGoodTuringSmoothing():
+    '''
+        Controller function'''
+
+if __name__ == '__main__':
+    applyGoodTuringSmoothing()
